506b27328a620e219a828fda0ae7d81d61fea992
Chanel Henley <chanel.henley@gocanvas.com>
2019-09-08 01:04:44 -0400

    fixed issue with error on validator

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,12 +4,13 @@ class RecipeImageValidator < ActiveModel::Validator
         image_file_ext = record.recipe_image[record.recipe_image - 4, image_str_len - 1]
         unless image_file_ext == '.jpg' || image_file_ext == '.jpeg' || image_file_ext == '.gif' || image_file_ext == '.png' || image_file_ext == '.svg'
             record.errors[:recipe_image] << 'Image must be a JPG, JPEG, GIF, PNG, or SVG format'
+        end
     end
 
 end
 
 class Recipe < ApplicationRecord
-    include ActiveModel::Validator
+    include ActiveModel::Validations
 
     validates :title, presence: true
     validates :minutes, numericality: {greater_than_or_equal_to: 0, less_than: 1000 }
